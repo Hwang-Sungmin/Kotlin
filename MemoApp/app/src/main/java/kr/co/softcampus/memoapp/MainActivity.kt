a3b7d59b9a992b43c20652f8be5dce9895fd9150
dk_sungmin <sungmin_hwang@daekyo.co.kr>
2023-04-21 11:03:35 +0900

    10.App1_메모 추가 Activity 실행

--- MemoApp/app/src/main/java/kr/co/softcampus/memoapp/MainActivity.kt
+++ MemoApp/app/src/main/java/kr/co/softcampus/memoapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package kr.co.softcampus.memoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
+import android.view.Menu
+import android.view.MenuItem
 import kr.co.softcampus.memoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +23,31 @@ class MainActivity : AppCompatActivity() {
         
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val helper = DBHelper(this)
-        helper.writableDatabase.close()
-
+        
+        // 툴 바 설정
+        setSupportActionBar(binding.mainToolbar)
+        title = "메모앱"
+        
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            // 추가버튼
+            R.id.main_menu_add -> {
+                val memoIntent = Intent(this, MemoAddActivity::class.java)
+                startActivity(memoIntent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 
     override fun onResume() {
         super.onResume()
@@ -42,3 +65,4 @@ class MainActivity : AppCompatActivity() {
         print("[hsm511] MainActivity -> onDestroy()")
     }
 }
+
