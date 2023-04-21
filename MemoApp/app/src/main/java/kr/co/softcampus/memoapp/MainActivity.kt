6e384ab68a48a83443b93aca3014e142c91a1ca0
dk_sungmin <sungmin_hwang@daekyo.co.kr>
2023-04-21 10:23:40 +0900

    9. App1_데이터베이스 구성 : SQLite DB 생성

--- MemoApp/app/src/main/java/kr/co/softcampus/memoapp/MainActivity.kt
+++ MemoApp/app/src/main/java/kr/co/softcampus/memoapp/MainActivity.kt
@@ -12,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        print("[hsm511] MainActivity -> onCreate()")
+
         // 스플래쉬 화면 이후로 보여질 화면의 테마 설정
         SystemClock.sleep(3000)
         setTheme(R.style.Theme_MemoApp)
@@ -19,18 +21,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val helper = DBHelper(this)
+        helper.writableDatabase.close()
+
     }
 
     override fun onResume() {
         super.onResume()
+        print("[hsm511] MainActivity -> onResume()")
     }
 
     override fun onPause() {
         super.onPause()
+        print("[hsm511] MainActivity -> onPause()")
     }
 
 
     override fun onDestroy() {
         super.onDestroy()
+        print("[hsm511] MainActivity -> onDestroy()")
     }
 }
