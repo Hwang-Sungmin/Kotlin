506af07f2849d30d69fa24b6012bdc45d1fe027f
dk_sungmin <sungmin_hwang@daekyo.co.kr>
2023-04-25 14:09:15 +0900

    13.App1_글 읽는 화면 실행 14.App1_글 읽는 화면 구현

--- MemoApp/app/src/main/java/kr/co/softcampus/memoapp/MainActivity.kt
+++ MemoApp/app/src/main/java/kr/co/softcampus/memoapp/MainActivity.kt
@@ -7,6 +7,8 @@ import android.os.SystemClock
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -17,11 +19,14 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
-    //제목을 담을 ArrayList
+    // 제목을 담을 ArrayList
     val subject_list = ArrayList<String>()
 
     // 작성 날짜를 담을 ArrayList
     val date_list = ArrayList<String>()
+
+    // 메모의 번호를 담을 ArrayList
+    val idx_list = ArrayList<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +49,7 @@ class MainActivity : AppCompatActivity() {
         binding.mainRecycler.adapter = main_recycler_adapter
 
         binding.mainRecycler.layoutManager = LinearLayoutManager(this)
+
 
     }
 
@@ -73,13 +79,14 @@ class MainActivity : AppCompatActivity() {
         // 리스트를 비워준다.
         subject_list.clear()
         date_list.clear()
+        idx_list.clear()
 
         // 데이터 베이스 오픈
         val helper = DBHelper(this)
 
         // 쿼리문
         val sql = """
-            select memo_subject, memo_date
+            select memo_subject, memo_date, memo_idx
             from MemoTable
             order by memo_idx desc
         """.trimIndent()
@@ -91,13 +98,16 @@ class MainActivity : AppCompatActivity() {
             // 컬럼 index를 가져온다
             val idx1 = c1.getColumnIndex("memo_subject")
             val idx2 = c1.getColumnIndex("memo_date")
+            val idx3 = c1.getColumnIndex("memo_idx")
 
             // 데이터를 가져온다
             val memo_subject = c1.getString(idx1)
             val memo_date = c1.getString(idx2)
+            val memo_idx = c1.getInt(idx3)
 
             subject_list.add(memo_subject)
             date_list.add(memo_date)
+            idx_list.add(memo_idx)
             //Log.d("memo_app", memo_subject)
             //Log.d("memo_app", memo_date)
             //Log.d("memo_app", "------------------------------------")
@@ -113,6 +123,17 @@ class MainActivity : AppCompatActivity() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
             val mainRecyclerBinding = MainRecylerRowBinding.inflate(layoutInflater)
             val holder = ViewHolderClass(mainRecyclerBinding)
+
+            // 생성되는 항목 View의 가로 세로 길이를 설정
+            val layoutParams = RecyclerView.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, // 가로길이
+                ViewGroup.LayoutParams.WRAP_CONTENT // 세로길이
+            )
+            mainRecyclerBinding.root.layoutParams = layoutParams
+
+            // 항목 View에 이벤트를 설정
+            mainRecyclerBinding.root.setOnClickListener(holder)
+
             return holder
         }
 
@@ -127,10 +148,25 @@ class MainActivity : AppCompatActivity() {
 
         // HolderClass
         inner class ViewHolderClass(mainRecyclerBinding: MainRecylerRowBinding) :
-            RecyclerView.ViewHolder(mainRecyclerBinding.root) {
+            RecyclerView.ViewHolder(mainRecyclerBinding.root), OnClickListener {
             // View의 주소값을 담는다.
             val rowMemoSubject = mainRecyclerBinding.memoSubject
             val rowMemoDate = mainRecyclerBinding.memoDate
+
+            // 리사이클러뷰의 항목하나에 이벤트를 주기 위해서. idx 번호 파악 가능
+            override fun onClick(v: View?) {
+                // Log.d("memo_app" , "항목 클릭 : $adapterPosition")
+                // 글 읽는 Activity를 실행한다.
+
+                // 현재 항목 글의 index를 가져온다.
+                val memo_idx = idx_list[adapterPosition]
+                //Log.d("memo_app", "memo_idx $memo_idx")
+                val memoReadAdapter = Intent(baseContext, MemoReadActivity::class.java)
+                memoReadAdapter.putExtra("memo_idx", memo_idx)
+                startActivity(memoReadAdapter)
+            }
+
+
         }
 
     }
---
+++ MemoApp/app/src/main/java/kr/co/softcampus/memoapp/MemoReadActivity.kt
@@ -0,0 +1,80 @@
+package kr.co.softcampus.memoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.core.graphics.createBitmap
+import kr.co.softcampus.memoapp.databinding.ActivityMemoReadBinding
+
+class MemoReadActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMemoReadBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMemoReadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.memoReadToolbar)
+        title = "메모 읽기"
+
+        // 툴바에 홈버튼
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // 데이터 베이스 값 가져오기
+        val helper = DBHelper(this)
+
+        val sql = """
+            select memo_subject, memo_date, memo_text
+            from MemoTable
+            where memo_idx = ?
+        """.trimIndent()
+
+        // 글 번호 추철
+        val memo_idx = intent.getIntExtra("memo_idx", 0)
+        
+        // 쿼리 실행
+        val args = arrayOf(memo_idx.toString())
+        val c1 = helper.writableDatabase.rawQuery(sql, args)
+        c1.moveToNext()
+
+        // 글 데이터를 추출
+        val idx1 = c1.getColumnIndex("memo_subject")
+        val idx2 = c1.getColumnIndex("memo_date")
+        val idx3 = c1.getColumnIndex("memo_text")
+
+        val memo_subject = c1.getString(idx1)
+        val momo_date = c1.getString(idx2)
+        val memo_text = c1.getString(idx3)
+
+        helper.writableDatabase.close()
+        //Log.d("memo_app", memo_subject)
+        //Log.d("memo_app", momo_date)
+        //Log.d("memo_app", memo_text)
+
+        binding.memoReadSubject.text = "제목 : $memo_subject"
+        binding.memoReadDate.text = "작성날짜 : $momo_date"
+        binding.memoReadText.text = memo_text
+
+    }
+
+    // 홈 버튼 누르기 할 때 뒤로가기 기능 추가
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+}
